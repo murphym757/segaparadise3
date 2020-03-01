4ab8bc7b4823a2e7d764751a5c61abed168741e2
Maurice Murphy <murphym757@yahoo.com>
2020-03-01 11:12:57 -0500

    Removed Material-Ui

--- src/app/components/screens/gameCollectionScreen.js
+++ src/app/components/screens/gameCollectionScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, Image } from 'react-native';
+import Button from '@material-ui/core/Button';
 import Font from '../../assets/styles/globalStyling';
 import axios from 'axios';
 import IGDB_API_KEY from '../../../server/config';
@@ -9,7 +10,14 @@ export default function GameCollectionScreen() {
     apiKey: IGDB_API_KEY.IGDB_API_KEY,
     game: {
       consoleID: 29,
-      id: ""
+      id: "",
+      name: "",
+      cover: "",
+      releaseDate: "",
+      genres: "",
+      developers:"",
+      players: "",
+      popularity: ""
     }
   });
 
@@ -27,7 +35,14 @@ export default function GameCollectionScreen() {
       .then(res => {
         setGameState({
           game: {
-            id: ""+ res.data[0].id +""
+            id: ""+ res.data[0].id + "",
+            name: ""+ res.data[0].name + "",
+            cover: ""+ res.data[0].cover + "",
+            releaseDate: ""+ res.data[0].first_release_date + "",
+            genres: ""+ res.data[0].genres + "",
+            developers: "" + res.data[0].involved_companies + "",
+            players: "" + res.data[0].player_perspectives + "",
+            popularity: "" + res.data[0].player_perspectives + ""
           }
         })
       })
@@ -42,8 +57,12 @@ export default function GameCollectionScreen() {
   
   return (
       <View>
-    <Text style={{fontFamily: 'Roboto-Regular'}}>{gameState.game.id}</Text>
-    </View>
+        <Text style={{fontFamily: 'Roboto-Regular'}}>{gameState.game.id}</Text>
+        <Text style={{fontFamily: 'Roboto-Regular'}}>{gameState.game.name}</Text>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </View>
   );
 }
 
